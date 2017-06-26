df2933123caa414bd39ef752d8e4836f5996a3b0
z <2091469944@qq.com>
2017-06-26 21:23:43 +0800

    自增 定义默认值 增加索引

--- testmysql.php
+++ testmysql.php
@@ -30,4 +30,26 @@
     	echo("创建数据表成功");
     	echo("<br>");
     }
+
+
+    $sql2="CREATE TABLE user2(
+    uid mediumint(8) unsigned NOT NULL auto_increment,
+    username char(15) NOT NULL default'',
+    password char(32) NOT NULL default'',
+    email varchar(40) NOT NULL default'',
+    regdate int(10) unsigned NOT NULL default 0,
+    PRIMARY KEY(uid),
+    UNIQUE KEY username(username),
+    KEY email(email)
+    )ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
+
+    if(mysql_query($sql2,$conn)){
+    	echo("创建数据表成功");
+    	echo("<br>");
+    }else{
+    	echo("创建数据表失败".mysql_error());
+    	echo("<br>");
+
+    }
+
 ?>
